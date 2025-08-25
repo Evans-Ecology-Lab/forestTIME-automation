b92aef532e58146dd1b4af61ff7d1e22126163bd
Eric Scott <scottericr@gmail.com>
2025-08-25 14:16:54 -0700

    try setting release tag from r script instead

--- update_metadata.R
+++ update_metadata.R
@@ -1,10 +1,20 @@
 library(cffr)
 library(lubridate)
 cff <- cff_read("CITATION.cff")
+# get previous release from GitHub
+prev_release <- system("git describe --abbrev=0 --tags", intern = TRUE)
+# prev_release <- "2024-08-25_1"
 
-release <- Sys.getenv("RELEASE", unset = stop("No release tag set!"))
+if (prev_release == today()) {
+  release <- paste(today(), 1, sep = "_")
+} else {
+  num <- as.numeric(strsplit(prev_release, split = "_")[[1]][2]) + 1
+  release <- paste(today(), num, sep = "_")
+}
 
-cff$version <- release
 cff$`date-released` <- format_ISO8601(today())
+cff$version <- release
 
 cff_write(cff)
+
+Sys.setenv(RELEASE = release)
