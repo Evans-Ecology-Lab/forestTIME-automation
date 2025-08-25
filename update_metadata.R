cf64ce8a8ed6195301cbf7d9d3939d758b22b720
Eric Scott <scottericr@gmail.com>
2025-08-25 13:38:13 -0700

    try release

--- update_metadata.R
+++ update_metadata.R
@@ -2,7 +2,9 @@ library(cffr)
 library(lubridate)
 cff <- cff_read("CITATION.cff")
 
+release <- Sys.getenv("RELEASE", unset = stop("No release tag set!"))
+
+cff$version <- release
 cff$`date-released` <- format_ISO8601(today())
-cff$version <- format_ISO8601(now())
 
 cff_write(cff)
