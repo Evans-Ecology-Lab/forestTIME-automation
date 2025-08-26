765829c8a493636116fe975f3e4a8be6ceed58fe
Eric Scott <scottericr@gmail.com>
2025-08-25 17:30:34 -0700

    try parsing prev versino better

--- update_metadata.R
+++ update_metadata.R
@@ -2,13 +2,26 @@ library(cffr)
 library(lubridate)
 cff <- cff_read("CITATION.cff")
 
-prev_release <- ymd(Sys.getenv("PREV_RELEASE"))
+prev_release <- Sys.getenv("PREV_RELEASE")
 
-if (prev_release == today()) {
-  release <- paste(today(), 1, sep = "_")
+#possibilitiies
+# prev_release <- as.character(today()) #one prev release today
+# prev_release <- paste(today(), 1, sep = "_") #already a manual release today
+# prev_release <- as.character(today() - 1) #release on a prior day
+# prev_release <- paste(today() - 1, 1, sep = "_") #manual release on a prior day
+
+if (grepl("_\\d+$", prev_release)) {
+  split <- strsplit(prev_release, "_")[[1]]
+  prev_date <- ymd(split[[1]])
+  num <- as.integer(split[[2]])
 } else {
-  num <- as.numeric(strsplit(prev_release, split = "_")[[1]][2]) + 1
-  release <- paste(today(), num, sep = "_")
+  prev_date <- ymd(prev_release)
+}
+
+if (prev_date != today()) {
+  release <- today()
+} else {
+  release <- paste(today(), num + 1L, sep = "_")
 }
 
 cff$`date-released` <- format_ISO8601(today())
@@ -16,6 +29,7 @@ cff$version <- release
 
 cff_write(cff)
 
-# Sys.setenv(RELEASE = release)
+Sys.setenv(RELEASE = release)
 
+#only works on GitHub
 system('echo "RELEASE=$RELEASE" >> $GITHUB_ENV')
