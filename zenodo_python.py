6f32e3fbabb2ae5620d2781ff7a73aa032034038
Devin Bayly <baylyd@email.arizona.edu>
2026-06-10 16:32:48 -0700

    Create zenodo_python.py

---
+++ zenodo_python.py
@@ -0,0 +1,19 @@
+import requests
+import os
+ACCESS_TOKEN=os.environ["ZENODO_TOKEN"]
+
+headers = {"Content-Type":"application/json","Authorization":f"Bearer {ACCESS_TOKEN}"}
+
+r= requests.post("https://zenodo.org/api/deposit/depositions',json={},headers=headers)
+
+bucket_url = r.json()["links"]["bucket"]
+
+filename = "GA.zip"
+
+path= "./%s" % filename
+
+headers=  {"Authorization":"Bearer {ACCESS_TOKEN}"}
+
+with open(path,"rb") as fp:
+  r = requests.put("%s/%s" %(bucket_url,filename), data=fp,headers=headers)
+print(r.json())
