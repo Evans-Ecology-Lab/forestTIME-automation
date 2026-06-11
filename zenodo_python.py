6b942bb1f2995af72c63eb071f2a7cd013cef441
Devin Bayly <baylyd@email.arizona.edu>
2026-06-10 18:18:41 -0700

    change trigger and add query to zenodo work

--- zenodo_python.py
+++ zenodo_python.py
@@ -4,9 +4,14 @@ import os
 ACCESS_TOKEN=os.environ["ZENODO_TOKEN"]
 print(ACCESS_TOKEN)
 
+# try to list out the existing r
+
 headers = {"Content-Type":"application/json","Authorization":f"Bearer {ACCESS_TOKEN}"}
 
-r= requests.post("https://zenodo.org/api/deposit/depositions",json={},headers=headers)
+r= requests.post("https://zenodo.org/api/deposit/depositions",params={"status":"draft","sort":"mostrecent","q":"17088642"},json={},headers=headers)
+print(r.json())
+
+
 
 bucket_url = r.json()["links"]["bucket"]
 
