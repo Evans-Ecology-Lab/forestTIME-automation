4df1da88cb01b6e4221f5bc2e89c0bbe3d4fdd55
Devin Bayly <baylyd@email.arizona.edu>
2026-06-10 17:52:33 -0700

    variable name wrong

--- zenodo_python.py
+++ zenodo_python.py
@@ -16,5 +16,5 @@ parent = contents[0].parent
 headers=  {"Authorization":"Bearer {ACCESS_TOKEN}"}
 
 with open(contents[0],"rb") as fp:
-  r = requests.put("%s/%s" %(bucket_url,filename), data=fp,headers=headers)
+  r = requests.put("%s/%s" %(bucket_url,fname), data=fp,headers=headers)
 print(r.json())
