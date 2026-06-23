363f0ea8ed11db5b71922f5752aee7b9d9984328
Devin Bayly <baylyd@email.arizona.edu>
2026-06-23 09:36:28 -0700

    change to check whether re-organization works

--- zenodo-api.py
+++ zenodo-api.py
@@ -4,137 +4,156 @@ import os
 import datetime
 import json
 import time
-ACCESS_TOKEN=os.environ["ZENODO_TOKEN"]
+import shutil as sh
+# make new folders that hold the 4 parquet files that are available
+parquets = sort(Path("fia/parquet").glob("*.parquet"))
 
-# try to list out the existing r
+# make the folders
+for p in parquets:
+  # the first part of the name separated by underscores is the state part
 
-conceptid = "17088642"
-headers = {"Authorization":f"Bearer {ACCESS_TOKEN}"}
+  state_name = p.name.split("_")[0]
+  folder = Path(f"fia/parquet/{state_name}")
+  folder.mkdir(exist_ok=True)
+  # try to move the file into that folder
+  sh.move(p,f"{folder}/{p.name}")
+  print("moved",p)
 
-r= requests.get("https://zenodo.org/api/deposit/depositions",params={"status":"draft","q":conceptid},headers=headers)
-print(r.json())
-
-# go through list and remove any  drafts, new versions or otherwise
-resultj = r.json()
-
-for res in resultj:
-  print("removing",res)
-  draft_id = res["id"]
-  r= requests.delete(f"https://zenodo.org/api/deposit/depositions/{draft_id}",headers=headers)
-  print()
-
-
-# get the latest version
-#gid = "20635476"
-gid = "17088643"
-r = requests.get(f"https://zenodo.org/api/deposit/depositions/{gid}",headers=headers)
-# 401
-res = r.json()
-res["links"]["latest"]
-r = requests.get( res["links"]["latest_draft"] ,headers=headers)
-res = r.json()
-print(res)
-print(res["id"])
-latest_id = res["id"]
-
-# make a new version
+new_locations = sort(Path("fia/parquet").glob("*/*.parquet"))
+print(new_locations)
 
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {ACCESS_TOKEN}"
-}
-print(latest_id)
-r = requests.post(f'https://zenodo.org/api/deposit/depositions/{latest_id}/actions/newversion',
-                   headers=headers)
+# ACCESS_TOKEN=os.environ["ZENODO_TOKEN"]
 
-# update the metadata
+# # try to list out the existing r
 
-print("updating metadata")
-new_version_res = r.json()
-print(new_version_res)
-meta = new_version_res["metadata"]
-x = datetime.datetime.now()
-meta["publication_date"]= x.strftime("%Y-%m-%d")
-meta["version"] = meta["publication_date"] 
+# conceptid = "17088642"
+# headers = {"Authorization":f"Bearer {ACCESS_TOKEN}"}
 
+# r= requests.get("https://zenodo.org/api/deposit/depositions",params={"status":"draft","q":conceptid},headers=headers)
+# print(r.json())
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {ACCESS_TOKEN}"
-}
-print(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}')
-r = requests.put(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}',
-                   data=json.dumps({"metadata":meta}),
-                   headers=headers)
+# # go through list and remove any  drafts, new versions or otherwise
+# resultj = r.json()
+
+# for res in resultj:
+#   print("removing",res)
+#   draft_id = res["id"]
+#   r= requests.delete(f"https://zenodo.org/api/deposit/depositions/{draft_id}",headers=headers)
+#   print()
 
 
+# # get the latest version
+# #gid = "20635476"
+# gid = "17088643"
+# r = requests.get(f"https://zenodo.org/api/deposit/depositions/{gid}",headers=headers)
+# # 401
+# res = r.json()
+# res["links"]["latest"]
+# r = requests.get( res["links"]["latest_draft"] ,headers=headers)
+# res = r.json()
+# print(res)
+# print(res["id"])
+# latest_id = res["id"]
+
+# # make a new version
 
 
-# delete the files in the new version draft
+# headers = {
+#     "Content-Type": "application/json",
+#     "Authorization": f"Bearer {ACCESS_TOKEN}"
+# }
+# print(latest_id)
+# r = requests.post(f'https://zenodo.org/api/deposit/depositions/{latest_id}/actions/newversion',
+#                    headers=headers)
 
-print("trying to delete contents of previous version")
-r = requests.get(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}/files',headers=headers)
-for f in r.json():
-    try:
-      r = requests.delete(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}/files/{f["id"]}',headers=headers)
+# # update the metadata
 
-    except Exception as e:
-      print("ran into ")
-      print(e)
-      print()
-      print("continuing")
+# print("updating metadata")
+# new_version_res = r.json()
+# print(new_version_res)
+# meta = new_version_res["metadata"]
+# x = datetime.datetime.now()
+# meta["publication_date"]= x.strftime("%Y-%m-%d")
+# meta["version"] = meta["publication_date"] 
 
-# adding new contents
-print("adding new contents")
-r = requests.get(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}',headers=headers)
-print(r)
-new_bucket_url = r.json()["links"]["bucket"]
-''' 
-The target URL is a combination of the bucket link with the desired filename
-seperated by a slash.
-'''
-files = sorted(Path("fia/parquet/").iterdir())
-print(files)
-headers = {
-    "Content-Type": "application/octet-stream",
-    "Authorization": f"Bearer {ACCESS_TOKEN}"
-}
-for f in files:
-  print("starting",f)
-  with open(f, "rb") as fp:
-    try:
-      r = requests.put(
-          "%s/%s" % (new_bucket_url,f.name),
-          data=fp,
-          headers=headers,
-      )
-      print(r)
-      print(r.json())
-    except Exception as e:
-      print("upload snag")
-      print(e)
-      print("retrying after delay")
-      failed = True
-      retries = 5
-      while failed:
-        retries -=1
-        if retries <0:
-          print("failed completely",f)
-          break
-        time.sleep(2)
-        try:
-          r = requests.put(
-              "%s/%s" % (new_bucket_url,f.name),
-              data=fp,
-              headers=headers,
-          )
-          print(r)
-          failed = False
-        except:
-          print("failed upload again",f)
+
+# headers = {
+#     "Content-Type": "application/json",
+#     "Authorization": f"Bearer {ACCESS_TOKEN}"
+# }
+# print(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}')
+# r = requests.put(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}',
+#                    data=json.dumps({"metadata":meta}),
+#                    headers=headers)
+
+
+
+
+# # delete the files in the new version draft
+
+# print("trying to delete contents of previous version")
+# r = requests.get(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}/files',headers=headers)
+# for f in r.json():
+#     try:
+#       r = requests.delete(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}/files/{f["id"]}',headers=headers)
+
+#     except Exception as e:
+#       print("ran into ")
+#       print(e)
+#       print()
+#       print("continuing")
+
+# # adding new contents
+# print("adding new contents")
+# r = requests.get(f'https://zenodo.org/api/deposit/depositions/{new_version_res["id"]}',headers=headers)
+# print(r)
+# new_bucket_url = r.json()["links"]["bucket"]
+# ''' 
+# The target URL is a combination of the bucket link with the desired filename
+# seperated by a slash.
+# '''
+# files = sorted(Path("fia/parquet/").iterdir())
+# print(files)
+# headers = {
+#     "Content-Type": "application/octet-stream",
+#     "Authorization": f"Bearer {ACCESS_TOKEN}"
+# }
+# for f in files:
+#   print("starting",f)
+#   with open(f, "rb") as fp:
+#     try:
+#       r = requests.put(
+#           "%s/%s" % (new_bucket_url,f.name),
+#           data=fp,
+#           headers=headers,
+#       )
+#       print(r)
+#       print(r.json())
+#     except Exception as e:
+#       print("upload snag")
+#       print(e)
+#       print("retrying after delay")
+#       failed = True
+#       retries = 5
+#       while failed:
+#         retries -=1
+#         if retries <0:
+#           print("failed completely",f)
+#           break
+#         time.sleep(2)
+#         try:
+#           r = requests.put(
+#               "%s/%s" % (new_bucket_url,f.name),
+#               data=fp,
+#               headers=headers,
+#           )
+#           print(r)
+#           failed = False
+#         except:
+#           print("failed upload again",f)
 
           
 
-    print("uploaded ",f)
-r.json()
+#     print("uploaded ",f)
+# r.json()
